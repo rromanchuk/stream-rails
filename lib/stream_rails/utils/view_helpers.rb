da3cf5dad929b16885da9eeea1a7d62a84e7662d
Ryan Romanchuk <rromanchuk@gmail.com>
2016-10-06 18:18:55 -0700

    experiment

--- lib/stream_rails/utils/view_helpers.rb
+++ lib/stream_rails/utils/view_helpers.rb
@@ -2,15 +2,15 @@
 module StreamRails
   # Module extending ActionView::Base and adding `render_activity` helper.
   module ViewHelpers
-    # View helper for rendering an activity
-    def render_activity(activity, options = {})
-      Renderable.render(activity, self, options)
-    end
+    # # View helper for rendering an activity
+    # def render_activity(activity, options = {})
+    #   Renderable.render(activity, self, options)
+    # end
 
-    # View helper for rendering many activities
-    def render_activities(activities, options = {})
-      activities.map { |activity| Renderable.render(activity, self, options.dup) }.join.html_safe
-    end
+    # # View helper for rendering many activities
+    # def render_activities(activities, options = {})
+    #   activities.map { |activity| Renderable.render(activity, self, options.dup) }.join.html_safe
+    # end
   end
   ActionView::Base.class_eval { include ViewHelpers }
 end
